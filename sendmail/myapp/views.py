85534d7a8e189c1b5cad99a75e231078930acb6a
Ismayil Dadashov <nemessita@gmail.com>
2024-01-06 00:31:33 +0400

    sendmail_django

---
+++ sendmail/myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def index(request):
+
+    if request.method == 'POST':
+        message = request.POST['message']
+        email = request.POST['email']
+        name = request.POST['name']
+        send_mail(
+            name,
+            message,
+            settings.EMAIL_HOST_USER,
+            [email],
+            fail_silently=False)
+        
+    return render(request, 'index.html')
+
+
+
